1ff387f5ae06a1a0ff2fdf4bede6d02131d4bab6
Kyland Evans <Kylandevans@gmail.com>
2022-10-15 15:01:03 -0400

    fixed nav, added routes to other pages

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,20 +1,22 @@
 const app = {
-	init: () => {
+	init() {
 		app.loadScript();
 	},
-	loadScript: () => {
+	loadScript() {
 		const navOpen = document.querySelector("[data-nav-open]");
 		const navClose = document.querySelector("[data-nav-close]");
 		const navbar = document.querySelector("[data-navbar]");
+		const htmlEle = document.querySelector("html");
+		const bodyEle = document.querySelector("body");
 
 		const toggleNavOpen = () => {
-			console.log(getComputedStyle(navbar));
-
 			if (getComputedStyle(navbar).display === "none") {
 				navbar.style.display = "flex";
 				navClose.style.display = "flex";
 				navOpen.style.display = "none";
 				navOpen.style.zIndex = 1;
+				htmlEle.classList.add("noscroll");
+				bodyEle.classList.add("noscroll");
 			}
 		};
 
@@ -24,21 +26,51 @@ const app = {
 				navOpen.style.display = "flex";
 				navbar.style.display = "none";
 				navOpen.style.zIndex = 10;
+				htmlEle.classList.remove("noscroll");
+				bodyEle.classList.remove("noscroll");
 			}
 		};
 
 		const navResizeEvent = () => {
 			if (window.innerWidth < 768) {
 				toggleNavClose();
+				htmlEle.classList.remove("noscroll");
+				bodyEle.classList.remove("noscroll");
 			} else if (window.innerWidth >= 768) {
 				toggleNavOpen();
 				navClose.style.display = "none";
+				htmlEle.classList.remove("noscroll");
+				bodyEle.classList.remove("noscroll");
 			}
 		};
 
 		navOpen.addEventListener("click", toggleNavOpen);
 		navClose.addEventListener("click", toggleNavClose);
 		window.addEventListener("resize", navResizeEvent);
+
+		const excusesText = document.querySelector("[data-excuses-text]");
+		const typewriter = new Typewriter(excusesText, {
+			loop: true,
+		});
+
+		typewriter
+			.pauseFor(1000)
+			.typeString("EXCUSES")
+			.pauseFor(2000)
+			.deleteAll(100)
+			.pauseFor(1000)
+			.typeString("CRYING")
+			.pauseFor(2000)
+			.deleteAll(100)
+			.pauseFor(1000)
+			.typeString("COMPLAINTS")
+			.pauseFor(2000)
+			.deleteAll(100)
+			.pauseFor(1000)
+			.typeString("WHINING")
+			.pauseFor(2000)
+			.deleteAll(100)
+			.start();
 	},
 };
 
